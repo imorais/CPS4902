4b317ac81cf4a9f2d14102a379771ac26879a9cb
imorais <moraisis@kean.edu>
2018-09-20 14:14:25 -0400

    Class that contains the value of a Factor

---
+++ homework/FactorValue.java
@@ -0,0 +1,36 @@
+
+public class FactorValue {
+	private String name;
+	private int amount;
+	
+	public FactorValue(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	// This is to compare factor values with each other.
+	public int hashCode() {
+		return name.hashCode();
+	}
+	public boolean equals(Object object) {
+		boolean returnValue = true;
+		if(object == null || (getClass() != object.getClass())) {
+			returnValue = false;
+		}
+		if(name == null || (((FactorValue) object).name != null)){
+			returnValue = false;
+		}
+		else if(!name.equals(((FactorValue) object).name)) {
+			returnValue = false;
+		}
+		return returnValue;
+	}
+
+}
