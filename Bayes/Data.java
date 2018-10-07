17e655de42ba54440ac7c4d722197c1223475462
imorais <moraisis@kean.edu>
2018-10-07 13:20:42 -0400

    Do #6 on Exam 1

--- Bayes/Data.java
+++ Bayes/Data.java
@@ -24,12 +24,33 @@ public class Data {
 			{"overcast",	 "hot", 		"normal", 	"F", 	"yes"},
 			{"rainy",	 "mild", 	"high",		"T", 	"no"}};
 	
+	static String [][] num6 = {{"Author", "Thread", "Length", "Where Read", "User Action"},
+			{"known",	"new",			"long",		"home", 		"skips"},		//1
+			{"unknown",	"new",			"short",		"work", 		"reads"},		//2
+			{"unknown",	"follow up",		"long", 		"work", 		"skips"},		//3
+			{"known",	"follow up",		"long",		"home", 		"skips"},		//4	
+			{"known",	"new",			"short",		"home", 		"reads"},		//5
+			{"known",	"follow up",		"long",		"work", 		"skips"},		//6
+			{"unknown",	"follow up",		"short", 	"work", 		"skips"},		//7
+			{"unknown",	"new", 			"short",		"work", 		"reads"},		//8
+			{"known",	"follow up",		"long", 		"home", 		"skips"},		//9
+			{"known",	"new", 			"long",		"work", 		"skips"},		//10
+			{"unknown",	"follow up",		"short", 	"home", 		"skips"},		//11
+			{"known",	"new",			"long",		"work", 		"skips"},		//12
+			{"known",	"follow up",		"short",		"home", 		"reads"},		//13
+			{"known",	"new",			"short",		"work", 		"reads"},		//14
+			{"known",	"new",			"short",		"home", 		"reads"},		//15
+			{"known",	"follow up",		"short",		"work", 		"reads"},		//16
+			{"known",	"new",			"short",		"home", 		"reads"},		//17
+			{"unknown",	"new",			"short",		"work", 		"reads"}};		//18
+	
 	// this map contain the data sets that can be used in this program
 	// Weather will be the default data set to be worked on first
 	static Map<String, String[][]> datas = Collections.unmodifiableMap(new HashMap<String, String[][]>() {
 		private static final long serialVersionUID = 1L;
 		{
 			put("Weather", Weather);
+			put("Num6", num6);
 		}
 	});
 
